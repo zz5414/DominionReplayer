a95839acbb1e42c149a2d040483e24d535fa7e5b
heejin.park <zzqq5414@gmail.com>
2020-12-12 11:04:44 +0900

    recharts 시작

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip} from 'recharts';
+import Template from './components/Template';
 
-function App() {
+const App = () => {
+  const data = [
+    {
+      name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+    },
+    {
+      name: 'Page B', uv: -3000, pv: 1398, amt: 2210,
+    },
+    {
+      name: 'Page C', uv: -2000, pv: -9800, amt: 2290,
+    },
+    {
+      name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+    },
+    {
+      name: 'Page E', uv: -1890, pv: 4800, amt: 2181,
+    },
+    {
+      name: 'Page F', uv: 2390, pv: -3800, amt: 2500,
+    },
+    {
+      name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+    },
+  ];
+
+  const [Data, setData] = useState(["동", "금", "은"]);
+  const [Test, setTest] = useState("None");
+
+
+  const handleClick = (data, index) => {
+    // if (data.activeLabel !== None) {
+      setTest(data.activeLabel);
+    // }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }} onClick={handleClick}>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+      </LineChart>
+
+      <Template>{Test}</Template>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Template.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Template.scss'
+
+const Template = ({children}) => {
+    return (
+        <div className="Template">
+            <div className="title"> Turn 4</div>
+            <div className="content"> {children} </div>
+        </div>
+    );
+};
+
+export default Template;
